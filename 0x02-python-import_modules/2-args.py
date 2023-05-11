300a626815c513f8b577cc54f467903f67fe22d1
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-11 09:48:32 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    argc = len(args) - 1
+
+    if len(args) == 1:
+        print(f"{0} arguments.")
+    elif len(args) == 2:
+        print("{} argument:\n{}: {}".format(1, 1, args[1]))
+    else:
+        print("{} arguments:".format(argc))
+
+    for arg in range(argc):
+        print("{}: {}".format(arg + 1, args[arg + 1]))
