0386fd94d7cea1d748e3bcd09a9d66756f33fe0e
Godbless Biekro <biekrogodbless@gmail.com>
2023-05-14 05:13:33 -0700

    fixi

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
         res = ''
         for char in my_string:
             if char != 'c' and char != 'C':
-                result += char
-        return result
+                res += char
+        return res
