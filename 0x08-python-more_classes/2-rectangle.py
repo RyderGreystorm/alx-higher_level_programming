b353c16d39e561c887e3fe34ef3c84c389d47f7f
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-05 18:21:41 -0700

    adding public methods to rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Adding public methods to the class"""
+
+
+class Rectangle:
+    """Rectangle Definition"""
+
+    def __init__(self, width=0, height=0):
+        """initializing method attributes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Finding the erea of the rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """perimeter calculation"""
+        return 2 * (self.__height + self.width)
