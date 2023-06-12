09034b2fd0def74bfedfff0f158c0f67535da35b
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-12 15:44:47 -0700

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,4 @@ def lookup(obj):
     Args:
         obj: object passed to function as params
     """
-    return [i for i in dir(obj) if not i.startswith('__')]
+    return [i for i in dir(obj) if i.startswith('__')]
