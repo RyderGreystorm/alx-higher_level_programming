7f78b8910dc497e83fd965b7c32903ea49872ed5
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-12 16:00:06 -0700

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+"""Accepts an object and returns attribuutes of that object"""
+
+
 def lookup(obj):
     """
     lookup - provides the list of availabe attributes of an object
     Args:
         obj: object passed to function as params
     """
-    return sorted([attr for attr in dir(obj)])
+    return dir(obj)
