126ff85ad3893f437bcab79df786c06afded85d4
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-12 19:08:02 -0700

    sublass finding

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Geometric class"""
+
+
+class BaseGeometry:
+    """
+    Geomtric base class
+    """
+    def area(self):
+        """ Area of the geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        """instance validator"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
