52bd0d5986186d93ca76ff3663f962a651917662
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-12 18:48:45 -0700

    sublass finding

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Geometric class"""
+
+
+class BaseGeometry:
+    """
+    Geomtric base class
+    """
+    def area(self):
+        """ Area of the geometry"""
+        raise Exception("arear() not implemented")
