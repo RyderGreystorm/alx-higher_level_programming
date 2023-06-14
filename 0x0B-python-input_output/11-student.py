9f07857d409f47736cd49f5f99506e5d8d84ea06
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-13 17:38:17 -0700

    obj to json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+student class and json conversion
+"""
+
+
+class Student:
+    """class definition"""
+    def __init__(self, first_name, last_name, age):
+        """instanciating other instances of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary reperssetation of a student
+        instance and returns it
+        """
+        if attrs is None:
+            attrs = self.__dict__.keys()
+        serialized_dic = {}
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                serialized_dic[key] = value
+
+        return serialized_dic
+
+    def reload_from_json(self, json):
+        """
+        eplaces all attributes of the Student instance
+        """
+
+        for key, value in json.items():
+            setattr(self, key, value)
