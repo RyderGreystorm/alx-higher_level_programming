fd852ef5596dc54f0486557d63fac5f7b65bf786
Godbless Biekro <biekrogodbless@gmail.com>
2023-06-19 15:23:02 -0700

    square update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base():
         Arg:
             list_dictionaries: argument to be converted to json
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
