63c8b65f2b91b7f97abc49858ece5038a902c09a
Godbless Biekro <biekrogodbless@gmail.com>
2023-07-19 17:38:57 -0700

    first commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- The database name will be passed as an argument of the mysql command
+-- If the table id_not_null already exists, script should not fail
+
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
