4317778f7acce638b533962029e5c97be4e601df
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-14 07:49:01 -0700

    done with number conversion

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (typeof process.argv[2] === 'string' && !isNaN(process.argv[2])){
-	console.log('My number:', Math.trunc(parseFloat(process.argv[2])));
+if (typeof process.argv[2] === 'string' && !isNaN(process.argv[2])) {
+  console.log('My number:', Math.trunc(parseFloat(process.argv[2])));
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
