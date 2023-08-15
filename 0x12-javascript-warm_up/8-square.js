a02536074d19749adc2726635c221336acf70de9
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-14 22:11:51 -0700

    array

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,11 +5,12 @@ const x = Math.trunc(parseFloat(process.argv[2]));
 if (typeof x === 'string' || isNaN(process.argv[2])) {
   console.log('Missing size');
 }
-
-for (let i = 0; i < x; i++) {
-  let row = '';
-  for (let j = 0; j < x; j++) {
-    row += 'x';
+if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    let row = '';
+    for (let j = 0; j < x; j++) {
+      row += 'x';
+    }
+    console.log(row);
   }
-  console.log(row);
 }
