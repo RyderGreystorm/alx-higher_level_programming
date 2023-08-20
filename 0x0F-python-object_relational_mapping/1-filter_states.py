670152b91c0f890a8250ab0b9164dd78377a8314
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 15:54:33 -0700

    find starting with n

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Filtering data tp return data matching  wildcard"""
+
+
+import sys
+import MySQLdb
+
+
+def run():
+    """Code definition"""
+
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            passwd=pswd,
+            db=db_name)
+
+    cursor = db.cursor()
+    query = r"SELECT * FROM states WHERE name  LIKE 'N%'"
+
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    run()
