cee96e60d0c4dfbc833f7d27cfd8bfb40bf3d6ce
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 15:12:05 -0700

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,26 +3,27 @@
 import sys
 import MySQLdb
 
-my_username = sys.argv[1]
-my_pwd = sys.argv[2]
-db_name = sys.argv[3]
+if __name__ == "__main__":
+    my_username = sys.argv[1]
+    my_pwd = sys.argv[2]
+    db_name = sys.argv[3]
 
-db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=my_username,
-        passwd=my_pwd,
-        db=db_name)
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=my_username,
+            passwd=my_pwd,
+            db=db_name)
 
-cursor = db.cursor()
-query = 'SELECT * FROM states ORDER BY id LIMIT 5'
-cursor.execute(query)
+    cursor = db.cursor()
+    query = 'SELECT * FROM states ORDER BY states.id LIMIT 5'
+    cursor.execute(query)
 
-results = cursor.fetchall()
+    results = cursor.fetchall()
 
-# Display results
-for row in results:
-    print(row)
+    # Display results
+    for row in results:
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
