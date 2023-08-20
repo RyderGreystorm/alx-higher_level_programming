39deb6cdbafd77776fb4204b4805fb7a2cf2463b
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 15:33:24 -0700

    Commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,12 +15,13 @@ def run():
 
     db = MySQLdb.connect(
             host='localhost',
+            port=3306,
             user=my_username,
             passwd=my_pwd,
             db=db_name)
 
     cursor = db.cursor()
-    query = r'SELECT * FROM states ORDER BY states.id LIMIT 5'
+    query = 'SELECT * FROM states ORDER BY states.id'
     cursor.execute(query)
 
     results = cursor.fetchall()
@@ -30,7 +31,7 @@ def run():
         print(row)
 
     cursor.close()
-
+    db.close()
 
 if __name__ == "__main__":
     run()
