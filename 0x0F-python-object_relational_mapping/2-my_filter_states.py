88a5769c6b06728ec6ad2377326de8ef9e438f41
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 16:17:58 -0700

    find starting with n

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Taking Search params from user"""
+
+
+import sys
+import MySQLdb
+
+
+def run():
+    """Uses arguments on cmd_line"""
+
+    u_name = sys.argv[1]
+    pswd = sys.argv[2]
+    db_name = sys.argv[3]
+    s_query = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            passwd=pswd,
+            db=db_name)
+
+    cursor = db.cursor()
+
+    query = r"SELECT * FROM states WHERE name = '{}'".format(s_query)
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+
+if __name__ == "__main__":
+    run()
