033326b848b5cc35877461cfd66b88d64b0127f6
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 19:15:40 -0700

    done

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""Execute some sql queries using sqlalchemy"""
+
+"""Execute some SQL queries using SQLAlchemy"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from model_state import Base, State
 
 
 def run():
     """Begin code execution"""
+
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3]
-                ),
-            pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # execute query
-    query = session.query(State).\
-        filter(State.name.like(r'%a%')).order_by(State.id)
-    session.delete(query.all())
-    session.commit()
+    # Execute query
+    states_to_delete = session.query(State).\
+            filter(State.name.like(r'%a%')).\
+            all()
 
+    for state in states_to_delete:
+        session.delete(state)
+
+    session.commit()
 
 if __name__ == '__main__':
     run()
