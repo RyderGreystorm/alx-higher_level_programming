6d7fd8f937840e05e122ebf6dc87097e64a997d2
Godbless Biekro <biekrogodbless@gmail.com>
2023-08-20 19:28:45 -0700

    done now

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,7 +12,9 @@ def run():
     """Begin code execution"""
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3]
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]
                 ),
             pool_pre_ping=True)
     Base.metadata.create_all(engine)
