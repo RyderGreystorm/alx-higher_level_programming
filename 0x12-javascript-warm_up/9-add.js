f6843ba6a1cdfe06869fef5e0496dbb80d2e7af0
Godbless Biekro <biekrogodbless@gmail.com>
2023-11-13 23:40:01 -0800

    first commit

--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function add (a, b) {
-  console.log(a + b);
-}
-
-const t = process.argv[2] ? parseInt(process.argv[2]) : NaN;
-const d = process.argv[3] ? parseInt(process.argv[3]) : NaN;
-
-add(t, d);
